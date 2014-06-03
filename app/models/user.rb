a53e02189ee77430da4aade99b01179ab9659abb
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-06-03 16:49:31 +0200

    added user model

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.name = auth["info"]["name"] || ""
+      user.avatar = auth["info"]["image"] || ""
+      user.oauth_token = auth["credentials"]["token"] || ""
+      user.oauth_secret = auth["credentials"]["secret"] || ""
+    end
+  end
+
+end
