9bb0e0404563dd43835873edb87d6fd3877d9c6e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-06-03 17:48:02 +0200

    added index page and root to index in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root 'sessions#index'
+
   match '/auth/:provider/callback' => 'sessions#create', via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 
